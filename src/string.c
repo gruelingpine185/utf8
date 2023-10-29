2c9df692087651691469f532201dc971e07c60bd
gruelingpine185 <66073333+gruelingpine185@users.noreply.github.com>
2023-10-29 16:32:36 -0400

    updated utf8_get_expected_bytes() parameter

--- src/string.c
+++ src/string.c
@@ -9,10 +9,10 @@
  *
  * Returns a value from 0 to 3 if successful, otherwise returns -1.
 */
-int utf8_get_expected_bytes(int _cp);
+int utf8_get_expected_bytes(uint32_t _cp);
 
 
-int utf8_get_expected_bytes(int _cp) {
+int utf8_get_expected_bytes(uint32_t _cp) {
     if((_cp & 0x80) == 0)    return 0;   // 0b0xxx xxxx
     if((_cp & 0xe0) == 0xc0) return 1;   // 0b110x xxxx
     if((_cp & 0xf0) == 0xe0) return 2;   // 0b1110 xxxx
